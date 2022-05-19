c8d13bc77cb11d1efc13cf068ddc1365595c974f
Chris Gonzalez <chrisgonza@gmail.com>
2022-05-19 14:13:59 -0500

    push nfl pickle files

--- nfl-scraping/nfl_scrape.py
+++ nfl-scraping/nfl_scrape.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 import pickle
+import re
 
 os.chdir('/Users/chrisgonzalez/web-scraping/nfl-scraping/')
 
@@ -114,8 +115,10 @@ for yearsi in years:
         years_tables.append(df_temp)
         
         # save years data into python list
+        """
         with open('years_'+str(yearsi)+'.pkl', 'wb') as f:
             pickle.dump(years_tables, f)
+        """
 #### end loop for years data ####
 
         
@@ -201,9 +204,79 @@ for tr in range(len(team_refs)):
     file_label=file_label.replace('.htm','')
     file_label=file_label.replace('/','_')
     file_label=file_label+'_games'
+    
+    """
     with open(file_label+'.pkl', 'wb') as f:
         pickle.dump(df_temp, f)
+    """
 #### schedule loop ends here ####
+
+
+#### coaches and gms/owners tables ####
+coaches_gms_refs=pd.DataFrame(columns=['values','label','team'])
+for tr in range(len(team_refs)):
+    print(team_refs[tr])
+    sched_url="https://www.pro-football-reference.com/"+team_refs[tr]
+    # grab main url
+    r = requests.get(sched_url)
+    text=r.content   # get request content
+    
+    # allow all tables to show, replace commented out code blocks
+    text=text.replace(b'<!--',b'')
+    text=text.replace(b'-->',b'')
+    
+    # parse fully viewed page
+    soup = BeautifulSoup(text, 'html.parser')
+    all_links = soup.select('p a[href]')
+    
+    all_links_labels = soup.select('p')
+    
+    ## search data for key staff data points ##
+    search_terms=["coach","coordinator","asst","stadium","owner","manager","offensive","defensive"]
+    for i in range(len(all_links_labels)):
+        
+        ## clean up position name 
+        head=list(all_links_labels[i].select('strong'))
+        if head==[]:
+            continue
+        
+        head=[str(x).replace('<strong>', '').replace(':</strong>', '').lower() for x in head]
+        head=head[0]
+        
+        ## retrieve only the data in the search_terms (positions we care about)
+        for j in range(len(search_terms)):
+            if search_terms[j] in head:
+                values=list(all_links_labels[i].select('a[href]'))
+                values=[str(x) for x in values]
+                if head=="other notable asst.":
+                    values=[x for x in values if "coaches" in x]
+                if head in ["offensive scheme","defensive alignment"]:
+                    result = re.search('> (.*)</p>', str(all_links_labels[i]))
+                    values=[result.group(1)]
+                    
+                df=pd.DataFrame(values)
+                df['label']=head
+                df['team']=team_refs[tr]
+                df.columns=['values','label','team']
+                coaches_gms_refs=pd.concat([coaches_gms_refs,df])
+                break
+                
+""" save data frame of data 
+with open('coaches_gms_refs_upto2020'+'.pkl', 'wb') as f:
+    pickle.dump(coaches_gms_refs, f)
+"""                
+                
+                    
+                
+                
+                
+    all_links_labels[11].select('strong')
+    values=all_links_labels[11].select('a[href]')
+
+
+
+
+
 
 
 ## team rosters loop
@@ -308,7 +381,7 @@ player_hrefs=list(set(player_hrefs))
 #player_hrefs_links=[]
 
 player_hrefs_df=pd.DataFrame()
-for pr in range(6695,len(player_hrefs)):
+for pr in range(0,len(player_hrefs)):
     player_url="https://www.pro-football-reference.com/"+player_hrefs[pr]
     # grab main url
     r = requests.get(player_url)
@@ -340,28 +413,145 @@ with open('player_hrefs_df.pkl', 'wb') as f:
     pickle.dump(player_hrefs_df, f)
             
     
-    
-    
-    
-    # find all table elements
-    parsed_table = soup.find_all('table')
-
-
-
-
 #### box score hrefs
 box_score_refs=[item for sublist in box_score_refs for item in sublist]
 box_score_refs=list(set(box_score_refs))
-          
-        
+"""          
 with open('box_score_refs.pkl', 'wb') as f:
     pickle.dump(box_score_refs, f)
+"""
+for br in range(4000,len(box_score_refs)):
+    print(box_score_refs[br])
+    boxscore_url="https://www.pro-football-reference.com/"+box_score_refs[br]
+    # grab main url
+    r = requests.get(boxscore_url)
+    text=r.content   # get request content
+    
+    # allow all tables to show, replace commented out code blocks
+    text=text.replace(b'<!--',b'')
+    text=text.replace(b'-->',b'')
+    
+    # parse fully viewed page
+    soup = BeautifulSoup(text, 'html.parser')
+    
+    # find all table elements
+    parsed_table = soup.find_all('table')
+    # find all tables
+    df=pd.read_html(text)
+    
+    ## we want these dataframes
+    frame_list=['player_offense','player_defense','returns','kicking',
+            'passing_advanced','rushing_advanced','receiving_advanced',
+            'defense_advanced','home_starters','vis_starters',
+            'home_snap_counts','vis_snap_counts']
+
+    box_score_dfs=[]
+    for i in range(len(parsed_table)):
+        # check
+        onetable=parsed_table[i]
+        df_temp=df[i]      
+        try:
+            ## general tables 
+            if onetable['id'] in frame_list:
+                table_id=onetable['id']
+                print(table_id)
+                # deal with column names that have multiple headers 
+                if str(type(df_temp.columns))=="<class 'pandas.core.indexes.multi.MultiIndex'>":
+                    col0=df_temp.columns.get_level_values(0).tolist()
+                    col1=df_temp.columns.get_level_values(1).tolist()
+                    col_final=[]
+                    for x,y in zip(col0,col1):
+                        if 'Unnamed' in x:
+                            x=''
+                        if 'Unnamed' in y:
+                            y=''
+                        if x=='':
+                            col_final.append(x+''+y)
+                        else:
+                            col_final.append(x+'_'+y)            
+                    df_temp.columns=col_final
+                
+                onetable=onetable.find('tbody')
+                # table structure logic
+                dat1 = onetable.find_all('td', attrs={'data-stat': 'player'})
+                dat2 = onetable.find_all('th', attrs={'data-stat': 'player'}) 
+                if len(dat1)>len(dat2):
+                    dat=dat1
+                else:
+                    dat=dat2
+                # loop here to grab all boxscore href
+                player_name=[]
+                player_href=[]
+                for j in range(len(dat)):
+                    try:
+                        player_name.append(dat[j].a.get_text())                
+                        player_href.append(dat[j].a.get('href'))
+                    except:
+                        pass
+                # remove asterisk points
+                player_name=[sub.replace("\+","") for sub in player_name]
+                player_name=[sub.replace("\*","") for sub in player_name]
+                player_name=[sub.strip() for sub in player_name]
+
+                df_temp['Player']=df_temp['Player'].replace({"\+":""},regex=True)
+                df_temp['Player']=df_temp['Player'].replace({"\*":""},regex=True)
+                df_temp['Player']=df_temp['Player'].str.strip()
+    
+                # filter pandas df to player names and add columns
+                df_temp=df_temp.loc[df_temp['Player'].isin(player_name),:]
+                df_temp['table_id']=table_id
+                df_temp['player_href']=player_href
+                df_temp['box_score_id']=box_score_refs[br]
+                
+                box_score_dfs.append(df_temp)
+                print(df_temp.shape)
+                
+                
+            ## play by play tables 
+            if onetable['id'] in ['pbp']:
+                table_id=onetable['id']
+                print(table_id)
+                # deal with column names that have multiple headers 
+                if str(type(df_temp.columns))=="<class 'pandas.core.indexes.multi.MultiIndex'>":
+                    col0=df_temp.columns.get_level_values(0).tolist()
+                    col1=df_temp.columns.get_level_values(1).tolist()
+                    col_final=[]
+                    for x,y in zip(col0,col1):
+                        if 'Unnamed' in x:
+                            x=''
+                        if 'Unnamed' in y:
+                            y=''
+                        if x=='':
+                            col_final.append(x+''+y)
+                        else:
+                            col_final.append(x+'_'+y)            
+                    df_temp.columns=col_final
+                    
+                # filter pandas df to player names and add columns
+                df_temp['table_id']=table_id
+                df_temp['box_score_id']=box_score_refs[br]
+            
+                box_score_dfs.append(df_temp)
+                print(df_temp.shape)
+                
+        except:
+            pass
+
+    file_label=box_score_refs[br].replace('/boxscores/','boxscores_')
+    file_label=file_label.replace('.htm','')
+    with open(file_label+'.pkl', 'wb') as f:
+        pickle.dump(box_score_dfs, f)
 
         
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+       
     
     ## year loop done ##
     print(years[yy])
