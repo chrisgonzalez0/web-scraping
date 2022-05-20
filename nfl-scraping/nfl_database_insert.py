eb34286b64a6765c8a4aa522e736dbe72fb57b1b
Chris Gonzalez <chrisgonza@gmail.com>
2022-05-20 15:18:36 -0500

    make files to dataframes to prepare for sql insert

--- nfl-scraping/nfl_database_insert.py
+++ nfl-scraping/nfl_database_insert.py
@@ -17,9 +17,6 @@ os.chdir('/Users/chrisgonzalez/web-scraping/nfl-scraping/')
 files=os.listdir()
 
 roster_files=[x for x in files if "roster" in x]
-games_files=[x for x in files if "games" in x]
-years_files=[x for x in files if "years" in x]
-
 ph = pd.read_pickle('player_hrefs_df.pkl')
 
 ## load sample roster files and create empty master data frames
@@ -41,6 +38,65 @@ for i in range(len(roster_files)):
 del(roster1_temp,roster2_temp,roster_dfs)  
   
 roster1.columns=['pos','player','age','yrs','gs','summary_of_player_stats','drafted_tm_rnd_yr','table_id','player_href','team_href']
+## player id
+split_str=[str(x).replace("/players/", "") for x in roster1['player_href'] ]
+split_str = [ re.search('/(.*).htm', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+roster1['player_id']=split_str
+## year
+split_str=[str(x).replace("/teams/", "") for x in roster1['team_href'] ]
+split_str = [ re.search('/(.*).htm', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+roster1['year']=split_str
+## team id 
+split_str=[str(x).replace("/teams", "") for x in roster1['team_href'] ]
+split_str = [ re.search('/(.*)/', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+roster1['team_id']=split_str
+
+
 roster2.columns=['number','player','age','pos','g','gs','wt','ht','college_univ','birthdate','yrs','av',
                  'drafted_tm_rnd_yr','salary','table_id','player_href','team_href']
+## player id
+split_str=[str(x).replace("/players/", "") for x in roster2['player_href'] ]
+split_str = [ re.search('/(.*).htm', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+roster2['player_id']=split_str
+## year
+split_str=[str(x).replace("/teams/", "") for x in roster2['team_href'] ]
+split_str = [ re.search('/(.*).htm', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+roster2['year']=split_str
+## team id 
+split_str=[str(x).replace("/teams", "") for x in roster2['team_href'] ]
+split_str = [ re.search('/(.*)/', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+roster2['team_id']=split_str
+
+
+
+#### player to college 
+ph.columns=['player_href','college_href']
+## player id
+split_str=[str(x).replace("/players/", "") for x in ph['player_href'] ]
+split_str = [ re.search('/(.*).htm', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str ]
+ph['player_id']=split_str
+## college id
+split_str=[str(x).replace("<a href=\"https://www.sports-reference.com/cfb/players", "") for x in ph['college_href'] ]
+split_str = [ re.search('/(.*).htm', x) for x in split_str ]
+split_str=[ x.group(1) for x in split_str if x]
+ph['college_id']=''
+ph['college_id'][ph['college_href']!=''] =split_str
+
+
+
+
+
+
+
+
+games_files=[x for x in files if "games" in x]
+years_files=[x for x in files if "years" in x]
+
 
